c11188fb0f08ddebaa12debb5ca20b80efb95165
Hyrum Contreras <hyrumcontreras@gmail.com>
2025-07-28 17:38:26 -0700

    [Story 2.1] Cambios al footer

---
+++ src/app/pages/profiles-page/profiles-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profiles-page',
+  imports: [],
+  templateUrl: './profiles-page.html',
+  styleUrl: './profiles-page.css'
+})
+export class ProfilesPage {
+
+}
